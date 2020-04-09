c9561a795dbf31d58ed32f25ac053e362888969f
Artemkulish <57228865+Artemkulish@users.noreply.github.com>
2020-04-09 13:07:09 +0300

    Update deploy.sh

--- deploy.sh
+++ deploy.sh
@@ -1,8 +1,9 @@
-#! /bin/bash
-
+command=up
 a=$(docker run --rm \
     -v /var/run/docker.sock:/var/run/docker.sock \
         -v "$PWD:$PWD" \
             -w="$PWD" \
-                docker/compose:1.24.0 up)
-$a                
+                docker/compose:1.24.0 $command)
+
+#x=up
+echo "$a"
