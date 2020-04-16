8e29d396a80695d622c2a703c50a02f6a80589e2
Artemkulish <57228865+Artemkulish@users.noreply.github.com>
2020-04-16 10:57:33 +0300

    Update deploy.sh

--- deploy.sh
+++ deploy.sh
@@ -1,5 +1,6 @@
 #! /bin/bash
 
+# In order to be able to use docker-compose in Google Container-Optimized OS 
 function docker-compose (){
 docker run --rm \
     -v /var/run/docker.sock:/var/run/docker.sock \
@@ -11,16 +12,20 @@ docker run --rm \
 # Array of services defined in docker-compose
 services=("zookeeper" "kafka" "identity" "vehicle" "trip" "payment" "messaging" "simulation")
 
+# Loop through the array
 for container in ${services[*]}
 do 
-    # Downloading new image
+    # Downloading a new image
     docker pull gitlab.svagworks.me:5050/root/demo_3:$container &>/dev/null
+        # If the image is updated, then, restart the container
         if docker images | awk '{print $2}' | awk 'NR==2' | grep $container &>/dev/null; then
             echo "Updating $container"
             docker-compose up --no-deps -d $container &>/dev/null &
+        # If the container is already up, then, print it out
         elif 
             docker ps | grep -q $container; then
             echo "Running $container";
+        # If the container is down, then, start it    
         else
             echo "Starting $container"
             docker-compose up --no-deps -d $container &>/dev/null &
